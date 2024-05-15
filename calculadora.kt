cff1a9b351f31090152536a33cbcb6f072e654f7
camilalimaz <166648338+camilalimaz@users.noreply.github.com>
2024-05-15 18:05:59 -0300

    Create calculadora.kt

---
+++ calculadora.kt
@@ -0,0 +1,14 @@
+class Calculadora {
+    fun somar(a: Int, b: Int): Int {
+        return a + b
+    }
+    fun subtrair(a: Int, b: Int): Int {
+        return a - b
+    }
+    fun multiplicar(a: Int, b: Int): Int {
+        return a * b
+    }
+    fun dividir(a: Int, b: Int): Int {
+       return a / b
+    }
+}
