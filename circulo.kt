bb6e2998b607183c8447ce43fa7bd95793e1e7ec
camilalimaz <166648338+camilalimaz@users.noreply.github.com>
2024-05-15 18:05:12 -0300

    Create circulo.kt

---
+++ circulo.kt
@@ -0,0 +1,8 @@
+class circulo(raio: Double){
+	fun calculaDiametro(raio: Double):Double{
+        return 3.14 * raio
+    } 
+    fun calculaCircunferencia(raio: Double):Double{
+        return 2 * 3.14 * raio
+    }
+}
